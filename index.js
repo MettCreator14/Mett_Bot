cccffc68bcdb4614f650b350fd024791314333a9
MettCreator14 <107059905+MettCreator14@users.noreply.github.com>
2022-06-08 16:43:35 +0200

    Upload File

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client({
     intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]
 })
 
-client.login("process.env.token");
+client.login(process.env.token);
 
 client.on("ready", () => {
     console.log("ONLINE");
